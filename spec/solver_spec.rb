32617c58761867f78cc3b160a72e2fcfa131aabc
prius2055 <prius2055@gmail.com>
2023-06-29 18:43:25 +0100

    Add test for factorial method which fails

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,13 @@
 # require 'rspec'
 require_relative 'solver'
 
-describe Solver do 
+describe Solver do
+    
+    describe '#factorial method' do
+        it 'returns the factorial of a number' do
+            solver = Solver.new
+            expect(solver.factorial(5)).to eq (120)
+        end
+    end
+     
 end
