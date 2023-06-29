b727a092ac404fe07e5b2cb768da565d4d05b347
prius2055 <prius2055@gmail.com>
2023-06-29 19:01:12 +0100

    update solver_spec.rb to test for negative argument, which is failing

--- spec/solver.rb
+++ spec/solver.rb
@@ -1,9 +1,13 @@
 class Solver
     def factorial(n)
         result = 1
+        if n == 0 
+            result
+        else  
         (1..n).each do |i|
             result = result * i
         end
         result
+        end
     end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,6 +7,7 @@ describe Solver do
         it 'returns the factorial of a number' do
             solver = Solver.new
             expect(solver.factorial(5)).to eq (120)
+            expect(solver.factorial(-1)).to raise_error ('Negative values not accepted')
         end
     end
      
