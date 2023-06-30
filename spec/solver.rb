cec55f85f060642fb1dafef70a93da0cb0f83c63
prius2055 <prius2055@gmail.com>
2023-06-30 02:25:29 +0100

    fix linters errors

--- spec/solver.rb
+++ spec/solver.rb
@@ -1,21 +1,32 @@
-class Solver
-    def factorial(n)
-        result = 1
-        if n == 0 
-            result
-        # elsif  n < 0
-        #     raise ArgumentError "Argument must be a non-negative integer"
-        
-        else
-            (1..n).each do |i|
-            result = result * i
-        end
-        
-        end
-        result
-    end
+# frozen_string_literal: true
 
-    def reverse(word)
-        word.reverse
+# The Solver class provides methods for solving various problems.
+class Solver
+  def factorial(num)
+    raise 'Argument must be a non-negative integer' if num.negative?
+
+    result = 1 if num.zero?
+
+    result = 1
+    (1..num).each do |i|
+      result *= i
     end
+    result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,27 +1,42 @@
+# frozen_string_literal: true
+
 require_relative 'solver'
 
 describe Solver do
-    
-    describe '#factorial method' do
-        it 'returns the factorial of a number' do
-            solver = Solver.new
-            expect(solver.factorial(5)).to eq (120)
-            # expect(solver.factorial(-1)).to raise_error ('Negative values not accepted')
-        end
+  let(:solver) { Solver.new }
+
+  describe '#factorial method' do
+    it 'returns the factorial 0 to be 1' do
+      expect(solver.factorial(0)).to eq(1)
     end
 
-    describe '#reverse method' do
-        it 'returns the reverse of a word passed in as argument' do
-            solver = Solver.new
-            expect(solver.reverse('hello')).to eq ('olleh')
-        end
+    it 'returns the factorial of a number greater than 0' do
+      expect(solver.factorial(5)).to eq(120)
     end
 
-    describe '#fizzbuzz method' do
-        it 'returns a string fizz,buzz,fizzbuzz or N' do
-            solver = Solver.new
-            expect(solver.fizzbuzz(N % 3 == 0)).to eq ('fizz')
-        end
+    it 'raises an exception for factorial of number less than 0' do
+      expect { solver.factorial(-1) }.to raise_error('Argument must be a non-negative integer')
     end
-     
+  end
+
+  describe '#reverse method' do
+    it 'returns the reverse of a word passed in as argument' do
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+  end
+
+  describe '#fizzbuzz method' do
+    it 'returns a string fizz when num is divisible by 3' do
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+    it 'returns a string buzzcwhen num is divisible by 5' do
+      expect(solver.fizzbuzz(100)).to eq('buzz')
+    end
+    it 'returns a string fizzbuzz when num is divisible by 3 and 5' do
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+    it 'returns a string of num for all other values not divisible by 3 or 5' do
+      expect(solver.fizzbuzz(14)).to eq('14')
+    end
+  end
 end
